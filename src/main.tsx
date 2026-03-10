584e98bb009b2e416e76cfd312a0cbe22c981e68
snow-prog-14 <alejandramaraasin0@gmail.com>
2026-03-10 13:35:46 +0800

    ThemeToggle

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,15 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+console.log("MAIN.TSX RUNNING ✅");
+import React from "react";
+import { createRoot } from "react-dom/client";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./index.css";
+import { initBackground } from "./background";
+import App from "./App";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
+initBackground();
+
+createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
     <App />
-  </StrictMode>,
-)
+  </React.StrictMode>,
+);
